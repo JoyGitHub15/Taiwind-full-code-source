1ae4d7845065aea6dfb4920c7fb4140154284ddd
Jahirul <mdjahirulislam931559@gmail.com>
2026-02-03 15:50:06 +0600

    my first project with tailwind

---
+++ project01/script.js
@@ -0,0 +1,7 @@
+let mIconElement=document.querySelector("#mIcone");
+let navElement=document.querySelector("nav");
+mIconElement.addEventListener("click",()=>{
+
+  navElement.classList.toggle("left-[0px]");
+
+})
