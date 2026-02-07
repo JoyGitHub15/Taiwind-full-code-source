5dbd7650086f1157b7aed44ccf2f2940fdb3e2ea
Jahirul <mdjahirulislam931559@gmail.com>
2026-02-07 17:48:20 +0600

    new file add

--- project02/javascript02.js
+++ project02/javascript02.js
@@ -1,9 +1,18 @@
-const sidebar = document.getElementById("sidebar");
+// const sidebar = document.getElementById("sidebar");
 
-function openMenu(){
-  sidebar.classList.remove("-translate-x-full");
+// function openMenu(){
+//   sidebar.classList.remove("-translate-x-full");
+// }
+
+// function closeMenu(){
+//   sidebar.classList.add("-translate-x-full");
+// }
+function openMenu() {
+  document.getElementById("sidebar")
+    .classList.remove("-translate-x-full");
 }
 
-function closeMenu(){
-  sidebar.classList.add("-translate-x-full");
+function closeMenu() {
+  document.getElementById("sidebar")
+    .classList.add("-translate-x-full");
 }
